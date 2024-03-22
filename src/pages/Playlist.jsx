b6aff8f904b74beffe1481dcf0eaf0ed8122ab52
Akshay Bhati <akshay@obvious.technology>
2024-03-22 12:17:54 +0530

    add drag and reorder functionality to the playlist

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import Thumbnail from "../components/Thumbnail";
 import VideoPlayer from "../components/VideoPlayer";
 import { videos } from "../utils/urls";
@@ -6,29 +7,69 @@ import { videos } from "../utils/urls";
 const Playlist = () => {
   const [showPlayer, setShowPlayer] = useState(false);
   const [index, setIndex] = useState(0);
+  const [list, setList] = useState(videos);
   const handleEsc = (event) => {
     if (event.key === "Escape") {
       setShowPlayer(false);
     }
   };
-  const playlist = videos.map((video) => video.videoUrl);
+
+  const onDragEnd = (result) => {
+    const newList = [...list];
+    const [removed] = newList.splice(result.source.index, 1);
+    newList.splice(result.destination.index, 0, removed);
+    setList(newList);
+  };
+
+  // const playlist = videos.map((video) => video.videoUrl);
   useEffect(() => {
     window.addEventListener("keydown", handleEsc);
   }, []);
   return (
-    <>
-      <div className="p-2 lg:p-14">
-        <h2 className="text-4xl font-bold mb-14">My Playlist</h2>
-        {videos.map((video) => (
-          <Thumbnail
-            video={video}
-            setShowPlayer={setShowPlayer}
-            setIndex={setIndex}
-          />
-        ))}
-      </div>
-      {showPlayer ? <VideoPlayer index={index} playlist={playlist} /> : <></>}
-    </>
+    <div className="p-2 lg:p-14">
+      <h2 className="text-4xl font-bold mb-14">My Playlist</h2>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              <div>
+                {list.map((video, index) => (
+                  <Draggable
+                    key={video.id}
+                    draggableId={video.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Thumbnail
+                          video={video}
+                          setShowPlayer={setShowPlayer}
+                          index={index}
+                          setIndex={setIndex}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+      {showPlayer ? (
+        <VideoPlayer
+          index={index}
+          playlist={list.map((video) => video.videoUrl)}
+        />
+      ) : (
+        <></>
+      )}
+    </div>
   );
 };
 
